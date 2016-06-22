6a1be0134a23d1de85b65f5c140e7d3a40cdb931
MastaMan <lukuanenko@gmail.com>
2016-06-22 14:47:48 +0300

    Update 1.0.6
    * Changed: data.js

--- js/data.js
+++ js/data.js
@@ -7,7 +7,7 @@
          nonIntegrated: { 1: 91, 2: 91, 3: 91, 4: 91, 5: 91, 6: 91, 7: 91, 8: 91, 9: 91, 10: 91 },
          integrated: { 1: 0, 2: 50, 3: 50, 4: 50, 5: 50, 6: 50, 7: 60, 8: 60, 9: 60, 10: 60 }	,
 		 highLight: [],
-		 desaturate: [2, 3, 4, 5]		 
+		 desaturate: [2, 3, 4, 5, 6, 7, 8, 9, 10]		 
      },
      {
          name: "Manifold / Plet",
@@ -16,7 +16,7 @@
          nonIntegrated: { 1: 30, 2: 30, 3: 30, 4: 30, 5: 30, 6: 30, 7: 30, 8: 30, 9: 30, 10: 30 },
          integrated: { 1: 0, 2: 0, 3: 35, 4: 45, 5: 45, 6: 45, 7: 45, 8: 45, 9: 55, 10: 55 },
          highLight: [],	
-		 desaturate: [3, 4, 5]	
+		 desaturate: [3, 4, 5, 6, 7, 8, 9, 10]	
      },	
      {
          name: "Controls",
@@ -25,7 +25,7 @@
          nonIntegrated: {1: 34, 2: 34, 3: 34, 4: 34, 5: 34, 6: 34, 7: 34, 8: 34, 9: 34, 10: 34},
          integrated: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0 },
 		 highLight: [],
-		 desaturate: [4, 5]
+		 desaturate: [4, 5, 6, 7, 8, 9, 10]
      },  
      {
          name: "TIE-IN SYSTEMS",
@@ -47,7 +47,7 @@ var table2 = [
          nonIntegrated: { 1: 83, 2: 83, 3: 83, 4: 83, 5: 83, 6: 83, 7: 83, 8: 83, 9: 83, 10: 83 },
          integrated: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 28, 7: 38, 8: 38, 9: 38, 10: 38 },
 		 highLight: [],
-		 desaturate: [6, 7, 8, 9]		
+		 desaturate: [6, 7, 8, 9, 10]		
      },
 	  {
          name: "Heavy Lift",
@@ -57,7 +57,7 @@ var table2 = [
          nonIntegrated: {1: 79, 2: 79, 3: 79, 4: 79, 5: 79, 6: 79, 7: 79, 8: 79, 9: 79, 10: 79},
          integrated: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 43, 8: 43, 9: 43, 10: 43 },
 		 highLight: [],
-		 desaturate: [7, 8, 9]	
+		 desaturate: [7, 8, 9, 10]	
      },
 	  {
          name: "Construction",
@@ -67,7 +67,7 @@ var table2 = [
          nonIntegrated: {1: 86, 2: 86, 3: 86, 4: 86, 5: 86, 6: 86, 7: 86, 8: 86, 9: 86, 10: 86},
          integrated: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 45, 9: 45, 10: 45 },
      	 highLight: [],
-		 desaturate: [8, 9]	
+		 desaturate: [8, 9, 10]	
      },
 	 {
          name: "Umbilical",
@@ -77,7 +77,7 @@ var table2 = [
          nonIntegrated: {1: 58, 2: 58, 3: 58, 4: 58, 5: 58, 6: 58, 7: 58, 8: 58, 9: 58, 10: 58},
          integrated: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 35, 10: 35 },
 		 highLight: [],
-		 desaturate: [9]			
+		 desaturate: [9, 10]			
      }
 ];
 
